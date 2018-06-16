766f5ea6837f3d363b6dccd15a3aa04df990832c
Wesley morais <wesley.genio@hotmail.com>
2018-06-16 14:55:59 -0300

    Novas activitys

---
+++ app/src/main/java/br/com/wesley/cestabsica/CadastrarActivity.java
@@ -0,0 +1,13 @@
+package br.com.wesley.cestabsica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar);
+    }
+}
